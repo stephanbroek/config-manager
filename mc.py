2a25b73b68ac8556b8cca86b976cd26c603a98c8
Stephan Broek <stephan@sbroek.com>
2015-08-07 19:42:25 +0200

    starting with init

--- mc.py
+++ mc.py
@@ -1,6 +1,29 @@
 import sys
+import csv
+import os
+
+HOME = os.path.expanduser("~")
 
 def main():
+	switch = {
+		'init': init,
+		'validate': validate
+	}
+	func = switch.get(sys.argv[1], lambda: printhelp)
+	return func()
+
+def init():
+	if os.path.exists( HOME + "/.config-manager/test"):
+		print 'Folder already exists, please validate it or check ~/.config-manager'
+		return 1
+
+	os.makedirs(HOME + "/.config-manager")
+
+	#with open('config.csv')
+	return 0
+
+def validate():
+	print 'val-hit'
 	return 0
 
 def printhelp():
@@ -8,7 +31,6 @@ def printhelp():
 	return 1
 
 if __name__ == '__main__':
-	print sys.argv
 	if len(sys.argv) == 1:
 		printhelp()
 	else:
